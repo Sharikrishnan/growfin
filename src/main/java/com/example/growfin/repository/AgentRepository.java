165a07c65fb6f790dbb9aa9b2a1a69d576f29b9d
Harikrishnan Senthil <sharikrishnan@athenahealth.com>
2021-04-25 15:52:11 +0530

    Code clean up

--- src/main/java/com/example/growfin/repository/AgentRepository.java
+++ src/main/java/com/example/growfin/repository/AgentRepository.java
@@ -4,8 +4,6 @@ import com.example.growfin.model.Agent;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Queue;
 
 @Repository
 public interface AgentRepository extends JpaRepository<Agent, Long> {
